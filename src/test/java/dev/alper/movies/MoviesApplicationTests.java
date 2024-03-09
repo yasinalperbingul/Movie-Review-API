e53812fee2d12939e2c2323e1bae8a61cce534c0
Yasin Alper Bingül <55395610+yasinalperbingul@users.noreply.github.com>
2024-03-09 16:18:27 +0300

    project initialized

---
+++ src/test/java/dev/alper/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alper.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
